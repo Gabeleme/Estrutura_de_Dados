544cfe3926cc52b4a97f34d41a6a875ba8692001
GabeLeme <gabioliveira_leme@outlook.com>
2024-08-30 19:07:11 -0300

    Versão2 Atualizada

--- Exercicios_aula1/src/atividade6.java
+++ Exercicios_aula1/src/atividade6.java
@@ -1,5 +1,20 @@
 public class atividade6 {
     public static void main(String[] args) {
-        
+        int[][] matriz = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+        };
+        matrizimprimir(matriz);
     }
+
+    public static void matrizimprimir(int[][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.println("|" + "\t" + matriz[i][j] + "\t" + "|");
+            }
+            System.out.println();
+        }
+    }
+
 }
--- Exercicios_aula1/src/atividade7.java
+++ Exercicios_aula1/src/atividade7.java
@@ -2,4 +2,7 @@ public class atividade7 {
     public static void main(String[] args) {
         
     }
+    public static void virarmatriz(int [][] matriz){
+        
+    }
 }
